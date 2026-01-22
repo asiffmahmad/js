5ca4397e408e09a7a6fc2dfc26d20a3ef8622750
asiff <asiffmahmad9@gmail.com>
2026-01-22 22:53:21 +0530

    basics

---
+++ Basics/DateSIn_Js.js
@@ -0,0 +1,6 @@
+let time =  new Date;
+// console.log(time);
+// console.log(time.toLocaleString('en-IN'));
+console.log(time.toDateString ());
+console.log(time.toTimeString ());
+console.log(time.toISOString ());
--- Basics/Number_math.js
+++ Basics/Number_math.js
@@ -14,3 +14,4 @@ let salary = 1000000
 console.log(Math.random(1.234));
 console.log(Math.floor(4.7));
 console.log(Math.ceil(4.2));  
+
